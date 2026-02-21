Fix backprop for distance function and add geo cbf training

diff_mfld_optim/geodesic/geodesic_funcs.py
@@ -34,14 +34,32 @@ def _log_map_so_approx(p, q, conn_coeffs):
 # with the above approximate methods
 
 
+# hack! open issue: https://github.com/pytorch/pytorch/issues/91810
+# NOTE: this just motivates not using torch as an autograd tool for evaluating
+# the function differentials in the future (especially when writing rust lib.)
+def detach_numpy(tensor):
+    tensor = tensor.detach().cpu()
+    if torch._C._functorch.is_gradtrackingtensor(tensor):
+        tensor = torch._C._functorch.get_unwrapped(tensor)
+
+        # deviating from the solution on the open issue for some reason the
+        # following can return a larger list (this could possibly be a
+        # security issue) so we just clip it to the length of the tensor
+        total_len = np.prod(tensor.shape)
+        raw_data = np.array(tensor.storage().tolist()[:total_len])
+        return np.array(raw_data).reshape(tensor.shape)
+    return tensor.numpy()
+
+
 class _ExpMapWrapper:
     def __init__(self, exp_map):
         self._exp_map = exp_map
 
     def __call__(self, p: torch.tensor, v: torch.tensor, conn: Connection):
         conn_coeffs = conn(p)
-        q = self._exp_map(np.asarray(p), np.asarray(v), np.asarray(conn_coeffs))
-        return torch.as_tensor(q)
+
+        q = self._exp_map(detach_numpy(p), detach_numpy(v), detach_numpy(conn_coeffs))
+        return torch.tensor(q, dtype=p.dtype, requires_grad=True)
 
 
 class _LogMapWrapper:
@@ -50,8 +68,9 @@ class _LogMapWrapper:
 
     def __call__(self, p: torch.tensor, q: torch.tensor, conn: Connection):
         conn_coeffs = conn(p)
-        v = self._log_map(np.asarray(p), np.asarray(q), np.asarray(conn_coeffs))
-        return torch.as_tensor(v)
+
+        v = self._log_map(detach_numpy(p), detach_numpy(q), detach_numpy(conn_coeffs))
+        return torch.tensor(v, dtype=p.dtype, requires_grad=True)
 
 
 class ExpMethod(Enum):
@@ -101,24 +120,42 @@ class DistSquaredMap(Function):
     # easily define the cost/constraint functions for an optimization problem
     # NOTE: do not use this as part of a training pipeline
 
+    generate_vmap_rule = True
+
     @staticmethod
     def forward(
-        ctx,
         p,
         q,
         metric: MetricField,
         conn: Connection = None,
         log_method=LogMethod.APPROX_SO,
     ):
+        # if a custom connection is not defined then use the levi-civita
+        # connection derived from the metric
+        if conn is None:
+            conn = metric.christoffels()
+
         g = metric(p)  # metric at point p
+
+        v = log_method(p, q, conn)  # tangent space at p
+        dist_sqr = g(v, v) ** 2
+
+        return dist_sqr
+
+    @staticmethod
+    def setup_context(ctx, inputs, output):
+        (p, q, metric, conn, log_method) = inputs
+
+        if conn is None:
+            conn = metric.christoffels()
+
+        g = metric(p)
         v = log_method(p, q, conn)  # tangent space at p
 
         dv = g.flat(v)  # cotangent space at p (differential)
         diff_dist_sqr = -2 * dv
-        ctx.save_for_backward(diff_dist_sqr)
 
-        dist_sqr = g(v, v) ** 2
-        return dist_sqr
+        ctx.save_for_backward(diff_dist_sqr)
 
     @staticmethod
     def backward(ctx, grad_output):
@@ -135,10 +172,65 @@ class DistSquaredMap(Function):
             None,
         )
 
+    # @staticmethod
+    # def setup_context(ctx, inputs, output):
+    #     # all work done in the forward pass
+    #     pass
+
+    # @staticmethod
+    # def forward(
+    #     ctx,
+    #     p: torch.Tensor,
+    #     q: torch.Tensor,
+    #     metric: MetricField,
+    #     conn: Connection = None,
+    #     log_method=LogMethod.APPROX_SO,
+    # ):
+    #     print(f"p_requires_grad={p.requires_grad}")
+    #     print(f"q_requires_grad={q.requires_grad}")
+
+    #     g = metric(p)  # metric at point p
+    #     v = log_method(p, q, conn)  # tangent space at p
+
+    #     print(f"v: {v.requires_grad}")
+
+    #     dv = g.flat(v)  # cotangent space at p (differential)
+    #     diff_dist_sqr = -2 * dv
+
+    #     diff_dist_sqr.requires_grad_()
+
+    #     # diff_dist_sqr.requires_grad=True
+
+    #     ctx.save_for_backward(diff_dist_sqr)
+
+    #     dist_sqr = g(v, v) ** 2
+
+    #     print(f"dist_sqr: {dist_sqr.requires_grad}")
+
+    #     dist_sqr.requires_grad_()
+    #     print(f"dist_sqr: {dist_sqr.requires_grad}")
+
+    #     return dist_sqr + 0 * p.sum()
+
+    # @staticmethod
+    # def backward(ctx, grad_output):
+    #     # dv is already the differential of the distance
+    #     (diff_dist_sqr,) = ctx.saved_tensors
+    #     return (
+    #         grad_output * diff_dist_sqr,
+    #         # torch needs a "gradient" for each input but these are just
+    #         # various parameters so we return None (non-differentiable)
+    #         None,
+    #         None,
+    #         None,
+    #         None,
+    #         None,
+    #     )
+
 
 def dist_squared_map(
-    p,
-    q,
+    p: torch.Tensor,
+    q: torch.Tensor,
     metric: MetricField,
     conn: Connection = None,
     log_method=LogMethod.APPROX_SO,

diff_mfld_optim/geometry/connection.py
@@ -19,9 +19,9 @@ class Connection(ABC):
     def _eval(self, p):
         pass
 
-    def __call__(self, *coords):
+    def __call__(self, *coords_sets):
         # in the case of product manifolds then we merge the coordinates
-        p = torch.cat(*coords, dim=0)
+        p = torch.cat(tuple(coords for coords in coords_sets))
         coords_n = p.shape[0]
         if coords_n != self.n:
             raise ValueError(

diff_mfld_optim/layers/diff_opt_layer.py
@@ -166,7 +166,7 @@ class DiffMfldOptimLayer(Module):
         num_batches = p0.shape[0]
         p_optimal_batched = torch.zeros_like(p0)
 
-        if not is_batched:
+        if pool is None:
             for i in range(num_batches):
                 p_optimal_batched[i, :] = DiffMfldOptimProblem.apply(
                     p0[i, :],
@@ -182,22 +182,24 @@ class DiffMfldOptimLayer(Module):
         else:
             # solves the optimization problem for all the batches included in
             # the inputs using the multiprocessing pool for maximum speed
-            p_optimal_results = pool.imap(
-                DiffMfldOptimProblem.apply,
-                [
-                    (
-                        p0[i, :],
-                        self.f,
-                        self.gs,
-                        self.hs,
-                        self.mfld_cfg,
-                        self.solve_cfg,
-                        self.method,
-                        *[arg[i, :] for arg in batched_args],
-                        *args,
-                    )
-                    for i in range(num_batches)
-                ],
+            p_optimal_results = list(
+                pool.imap(
+                    DiffMfldOptimProblem.apply,
+                    [
+                        (
+                            p0[i, :],
+                            self.f,
+                            self.gs,
+                            self.hs,
+                            self.mfld_cfg,
+                            self.solve_cfg,
+                            self.method,
+                            *[arg[i, :] for arg in batched_args],
+                            *args,
+                        )
+                        for i in range(num_batches)
+                    ],
+                )
             )
 
             # combines the results into the batched output

diff_mfld_optim/mfld_util.py
@@ -7,8 +7,9 @@ from diff_mfld_optim.geometry.connection import Connection
 from diff_mfld_optim.geodesic.geodesic_funcs import (
     ExpMethod,
     LogMethod,
-    dist_squared_map,
 )
+
+import diff_mfld_optim.geodesic.geodesic_funcs as geodesic_funcs
 
 
 @dataclass
@@ -26,4 +27,6 @@ class MfldCfg:
 
 def dist_squared_map(p, q, cfg: MfldCfg) -> torch.tensor:
     # allows clean mfldcfg interface for use in cost and constraint functions
-    return dist_squared_map(p, q, cfg.metric_field, cfg.conn, cfg.dist_method)
+    return geodesic_funcs.dist_squared_map(
+        p, q, cfg.metric_field, cfg.conn, cfg.dist_method
+    )

diff_mfld_optim/optim/constrained.py
@@ -1,5 +1,6 @@
 import torch
 
+from copy import deepcopy
 from enum import Enum
 from dataclasses import dataclass
 from typing import Union, List, Tuple
@@ -43,7 +44,8 @@ class ConstrainedSolverResult:
     constrs_violated: bool
     subsolver_failed: bool
     iters: int
-    p: torch.tensor
+    p: torch.Tensor
+    p0: torch.Tensor
     g_mults: List[Tuple[float, float]]  # g lagrange multipliers
     h_mults: List[Tuple[float, float]]  # h lagrange multipliers
     gs_eval: List[float]  # value of the constraints
@@ -83,8 +85,12 @@ def ralm(
     if solve_cfg.sub_cfg is None:
         raise ValueError("Subsolver configuration must be provided to use RALM")
 
+    # clones the constrained solver configuration so we can modify its
+    # properties without modifying the original template
+    solve_cfg = deepcopy(solve_cfg)
+
     p_prev = None
-    p = p0
+    p = p0.detach().clone()
 
     n = len(gs)  # number of inequalities
     m = len(hs)  # number of equalities
@@ -93,7 +99,6 @@ def ralm(
     h_mults = torch.zeros((m,))
 
     for i in range(solve_cfg.max_iters):
-        # print(f"i: {i}")
 
         # finds the point that minimizes the augmented lagrangian function with
         # with the current lagrangian multipliers
@@ -121,6 +126,7 @@ def ralm(
                 True,
                 i + 1,
                 p,
+                p0,
                 g_mults,
                 h_mults,
                 gs_eval,
@@ -150,6 +156,7 @@ def ralm(
                 False,
                 i + 1,
                 p,
+                p0,
                 g_mults,
                 h_mults,
                 gs_eval,
@@ -197,6 +204,7 @@ def ralm(
         True,
         solve_cfg.max_iters,
         p,
+        p0,
         g_mults,
         h_mults,
         gs_eval,

diff_mfld_optim/optim/subsolver.py
@@ -7,6 +7,8 @@ from typing import Callable, TypeVarTuple
 from diff_mfld_optim.mfld_util import MfldCfg
 from diff_mfld_optim.geodesic.geodesic_funcs import dist_map
 
+from torch.func import jacfwd, jacrev
+from torch.autograd.functional import jacobian
 
 FuncArgs = TypeVarTuple("FuncArgs")
 OptimFunc = Callable[[torch.Tensor, MfldCfg, *FuncArgs], torch.Tensor]
@@ -25,6 +27,7 @@ class SolverResult:
     success: bool
     iters: int
     p: torch.tensor
+    p0: torch.tensor
 
 
 def riem_grad_descent(
@@ -38,19 +41,16 @@ def riem_grad_descent(
     # available subsolvers to be used by ralm)
 
     p_prev = None
-    p: torch.Tensor = p0
+    p: torch.Tensor = p0.detach().clone()
 
     for i in range(solv_cfg.max_iters):
         # the jacobian takes too long so we abuse backward propagation here to
         # compute the differential of f (the gradient according to torch is
         # equivalent to differential in differential geometric terms)
-        p.requires_grad = True
-        p.grad = None
 
-        f(p, mfld_cfg, *args).backward()
-        df = p.grad.detach()
+        # print(f"subsolver: i={i}, p={p}")
 
-        p.requires_grad = False
+        df = jacrev(lambda p: f(p, mfld_cfg, *args))(p)
 
         # updates the point using the exponential map
         grad_f = mfld_cfg.metric_field(p).sharp(df)
@@ -67,14 +67,14 @@ def riem_grad_descent(
             )
             <= solv_cfg.conv_eps
         ):
-            return SolverResult(True, i + 1, p)
+            return SolverResult(True, i + 1, p, p0)
 
         p_prev = p.clone()  # otherwise it p == p_prev
 
         if solv_cfg.damp_growth is not None:
             solv_cfg.damp *= solv_cfg.damp_growth
 
-    return SolverResult(False, solv_cfg.max_iters, p)
+    return SolverResult(False, solv_cfg.max_iters, p, p0)
 
 
 class SubsolverMethod(Enum):

geo_diff_opt_layer_ml/training/gcbf.py
@@ -8,12 +8,7 @@ from diff_mfld_optim.mfld_util import MfldCfg, dist_squared_map
 def cbf_ko(
     p: torch.Tensor,  # current unicycle state
     u: torch.Tensor,  # current input
-    p_ko: torch.Tensor,  # ko centre position
-    dp_ko: torch.Tensor,  # ko centre velocity
-    ddp_ko: torch.Tensor,  # ko centre acceleration
-    r_ko: torch.Tensor,  # ko radius
-    dr_ko: torch.Tensor,  # ko radius velocity
-    ddr_ko: torch.Tensor,  # ko radius acceleration
+    ko: torch.Tensor,  # keep-out coniguration
     k_1: torch.Tensor,
     k_2: torch.Tensor,
 ):
@@ -21,19 +16,39 @@ def cbf_ko(
     # can be differentiated using autograd to be used in a constrained
     # optimization solver)
 
+    # NOTE: this is not intended as batch evaluation as it is intended to be
+    # wrapped as a function that is passed into the
+
     # for clarity expand the above elements (function signature kept in a more
     # convenient grouping rather than each individual element)
-    x, y, theta, v, omega = p[0], p[1], p[2], p[3], p[4]
-
-    x_ko, y_ko = p_ko[0], p_ko[1]
-    dx_ko, dy_ko = dp_ko[0], dp_ko[1]
-    ddx_ko, ddy_ko = ddp_ko[0], ddp_ko[1]
+    (x, y, theta, v, omega) = (p[0], p[1], p[2], p[3], p[4])
+    (
+        ko_x,
+        ko_y,
+        ko_vel_x,
+        ko_vel_y,
+        ko_accel_x,
+        ko_accel_y,
+        ko_radius,
+        ko_vel_radius,
+        ko_accel_radius,
+    ) = (
+        ko[0],
+        ko[1],
+        ko[2],
+        ko[3],
+        ko[4],
+        ko[5],
+        ko[6],
+        ko[7],
+        ko[8],
+    )
 
     u_f, _u_t = u[0], u[1]
 
     # for details refer to `dyn_ext_unicycle_ko_cbf_derivations.ipynb`
-    x_err = x_ko - x
-    y_err = y_ko - y
+    x_err = ko_x - x
+    y_err = ko_y - y
     r_sqr_err = x_err**2 + y_err**2
 
     r_1 = r_sqr_err ** (7.0 / 2) * x_err
@@ -47,26 +62,26 @@ def cbf_ko(
     a_coeff = -(r_1 * torch.cos(theta) + r_2 * torch.sin(theta)) / r_sqr_err**4 * u_f
     b_term = -(
         2 * k_1 * k_2 * r_sqr_err ** (9.0 / 2)
-        + 2 * k_1 * k_2 * r_sqr_err**4 * r_ko
-        - 2 * k_2 * r_1 * dx_ko
-        - 2 * k_2 * r_2 * dy_ko
-        + 2 * k_2 * r_sqr_err**4 * dr_ko
+        + 2 * k_1 * k_2 * r_sqr_err**4 * ko_radius
+        - 2 * k_2 * r_1 * ko_vel_x
+        - 2 * k_2 * r_2 * ko_vel_y
+        + 2 * k_2 * r_sqr_err**4 * ko_vel_radius
         - 2 * r_1 * torch.sin(theta) * v * omega
-        - 2 * r_1 * ddx_ko
+        - 2 * r_1 * ko_accel_x
         + 2 * r_2 * torch.cos(theta) * v * omega
-        - 2 * r_2 * ddy_ko
+        - 2 * r_2 * ko_accel_y
         + r_3 * torch.cos(2 * theta) * v**2
         + r_3 * v**3
-        + 2 * r_3 * ddx_ko**2
+        + 2 * r_3 * ko_accel_x**2
         - r_4 * torch.cos(2 * theta) * v**2
         + r_4 * v**2
-        + 2 * r_4 * (ddy_ko) ** 2
+        + 2 * r_4 * ko_accel_y**2
         + 2 * r_5 * torch.sin(2 * theta) * v**2
-        + 4 * r_5 * dx_ko * dy_ko
+        + 4 * r_5 * ko_vel_x * ko_vel_y
         - 2 * r_sqr_err ** (7.0 / 2) * v**2
-        - 2 * r_sqr_err ** (7.0 / 2) * (dx_ko) ** 2
-        - 2 * r_sqr_err ** (7.0 / 2) * (dy_ko) ** 2
-        + 2 * r_sqr_err**4 * ddr_ko
+        - 2 * r_sqr_err ** (7.0 / 2) * ko_vel_x ** 2
+        - 2 * r_sqr_err ** (7.0 / 2) * ko_vel_y ** 2
+        + 2 * r_sqr_err**4 * ko_accel_radius
     ) / (2 * r_sqr_err**4)
 
     return a_coeff + b_term
@@ -74,4 +89,4 @@ def cbf_ko(
 
 def gcbf_f(u: torch.Tensor, mfld_cfg: MfldCfg, u_nom: torch.Tensor):
     # minimization of correction between nominal control input and safe input
-    return 0.5 * dist_squared_map(u, u_nom, mfld_cfg)
+    return 0.5 * dist_squared_map(u, u_nom, mfld_cfg)  # >= 0